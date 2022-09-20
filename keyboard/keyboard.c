cfcf750276c3046816c726b73b66ff0fb1c31337
donarturo11 <arturwrona91@gmail.com>
2022-09-20 13:01:37 +0200

    Improve indentations

--- keyboard/keyboard.c
+++ keyboard/keyboard.c
@@ -2,47 +2,46 @@
 
 struct keyboard
 {
-	int active;
+    int active;
 };
 
 keyboard *keyboardInit()
 {
-	keyboard* k = NULL;
-	k = (keyboard*) malloc(sizeof(keyboard));
-	return k;
+    keyboard* k = NULL;
+    k = (keyboard*) malloc(sizeof(keyboard));
+    return k;
 }
 
 void keyboardDelete(keyboard *k)
 {
-	free(k);
+    free(k);
 }
 
 void keyboardLoop(keyboard *k)
 {
     k->active = TRUE;
-
     while (k->active){
-		keyboardDetect(k); 
+        keyboardDetect(k); 
     }
 }
 
 void keyboardDetect(keyboard *k)
 {
-	int keyCode=getch();
-	while (kbhit()){
-		keyCode+=getch();
-	}
-	keyboardShortcut(k, keyCode);
+    int keyCode=getch();
+    while (kbhit()){
+        keyCode+=getch();
+    }
+    keyboardShortcut(k, keyCode);
 }
 
 int keyboardShortcut(keyboard *k, int keyCode)
 {
     switch (keyCode) {
         case 'q': k->active = FALSE; break;
-		case K_UP: printf("UP\n"); break;
-		default: printf("KeyCode is %i\n", keyCode); break;
+        case K_UP: printf("UP\n"); break;
+        default: printf("KeyCode is %i\n", keyCode); break;
     }
-	return keyCode;
+    return keyCode;
 }
 
 void keyboardTest()
